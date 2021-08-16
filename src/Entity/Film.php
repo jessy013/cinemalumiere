ea3558bd4c8d3aa686fb7b8f3de884d8cf9a1956
jessy013 <84912053+jessy013@users.noreply.github.com>
2021-08-16 16:55:26 +0200

    mdr
    lol

---
+++ src/Entity/Film.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FilmRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FilmRepository::class)
+ */
+class Film
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $resume;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $anneeProduction;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $realisateur;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $listeActeur;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $imageUrl;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Genre::class, inversedBy="films")
+     */
+    private $genre;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Seance::class, mappedBy="film", orphanRemoval=true)
+     */
+    private $seances;
+
+    public function __construct()
+    {
+        $this->seances = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getResume(): ?string
+    {
+        return $this->resume;
+    }
+
+    public function setResume(string $resume): self
+    {
+        $this->resume = $resume;
+
+        return $this;
+    }
+
+    public function getAnneeProduction(): ?int
+    {
+        return $this->anneeProduction;
+    }
+
+    public function setAnneeProduction(int $anneeProduction): self
+    {
+        $this->anneeProduction = $anneeProduction;
+
+        return $this;
+    }
+
+    public function getRealisateur(): ?string
+    {
+        return $this->realisateur;
+    }
+
+    public function setRealisateur(string $realisateur): self
+    {
+        $this->realisateur = $realisateur;
+
+        return $this;
+    }
+
+    public function getListeActeur(): ?string
+    {
+        return $this->listeActeur;
+    }
+
+    public function setListeActeur(string $listeActeur): self
+    {
+        $this->listeActeur = $listeActeur;
+
+        return $this;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->imageUrl;
+    }
+
+    public function setImageUrl(string $imageUrl): self
+    {
+        $this->imageUrl = $imageUrl;
+
+        return $this;
+    }
+
+    public function getGenre(): ?Genre
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(?Genre $genre): self
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Seance[]
+     */
+    public function getSeances(): Collection
+    {
+        return $this->seances;
+    }
+
+    public function addSeance(Seance $seance): self
+    {
+        if (!$this->seances->contains($seance)) {
+            $this->seances[] = $seance;
+            $seance->setFilm($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSeance(Seance $seance): self
+    {
+        if ($this->seances->removeElement($seance)) {
+            // set the owning side to null (unless already changed)
+            if ($seance->getFilm() === $this) {
+                $seance->setFilm(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Service/FilmService.php
@@ -0,0 +1,35 @@
+<?php
+namespace App\Service;
+
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+
+class FilmService
+{
+    private $entityManager;
+    function __construct(EntityManagerInterface $em)
+    {
+        $this->entityManager = $em;
+    }
+    function ajouter()
+    {
+
+    }
+    function supprimer()
+    {
+
+    }
+    function recupererlist()
+    {
+
+    }
+    function recupererFilm()
+    {
+
+    }
+    function sauvegarder()
+    {
+
+    }
+    
+}
