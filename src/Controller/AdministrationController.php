133b350f64f4524f6dfe6a4d111466a1c670c0cd
jessy013 <84912053+jessy013@users.noreply.github.com>
2021-08-18 16:30:14 +0200

    holly sh**t

--- src/Controller/AdministrationController.php
+++ src/Controller/AdministrationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\GenreService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,6 +16,13 @@ class AdministrationController extends AbstractController
     {
         return $this->render('administration/index.html.twig', [
             'controller_name' => 'AdministrationController',
+        ]);
+    }
+    public function listeGenre(GenreService $genreService ):response
+    {
+        $genres = $genreService->liste();
+        return $this->render('administration/genres/liste_genres.html.twig', [
+            'genres' => $genres
         ]);
     }
 }
