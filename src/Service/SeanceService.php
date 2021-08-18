a0e841456681588f5e18e72b28265ef37e5f8f2a
jessy013 <84912053+jessy013@users.noreply.github.com>
2021-08-18 14:58:15 +0200

    Update SeanceService.php

--- src/Service/SeanceService.php
+++ src/Service/SeanceService.php
@@ -2,16 +2,17 @@
 namespace App\Service;
 
 use App\Entity\ICrud;
+use App\Entity\Seance;
 
 class SeanceService implements ICrud
 {
     public function liste()
     {
-        
+         return $this->entityManager->getRepository(Seance::class)->findAll();
     }
     public function lire($pId)
     {
-        
+        return $this->entityManager->getRepository(Seance::class)->find($pId);
     }
     public function sauvegarder()
     {
